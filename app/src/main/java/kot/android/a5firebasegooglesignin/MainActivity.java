820443fc9df82fe1bb11dcc372da26f0e66c9a7b
Admin <hubtth07@gmail.com>
2020-03-18 16:17:59 +0700

    Initial commit

---
+++ app/src/main/java/kot/android/a5firebasegooglesignin/MainActivity.java
@@ -0,0 +1,22 @@
+package kot.android.a5firebasegooglesignin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
